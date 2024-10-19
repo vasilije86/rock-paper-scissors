a32a5a99c18da0ba2e0b3f66f8908b0ebffad1d9
vasilije86 <vasilije.gto@gmail.com>
2024-10-19 22:30:58 +0200

    Created random computer choice

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "rock";
+    } else if (computerChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
